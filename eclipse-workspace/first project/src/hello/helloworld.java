e9ae344094d3f006e72f0b0474e75fab83ef443a
Sampadapant <sampada.0721@gmail.com>
2023-01-27 13:21:56 -0600

    first commit...

---
+++ eclipse-workspace/first project/src/hello/helloworld.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		String hungry= "yess";
+		
+		if (hungry == "yes");
+		System.out.println("eat something");
+     
+			System.out.println("then sleep");
+		}
+	}
+
+
